7f4cb691a211a74ef963eeecdd62c80333340fda
witnip <70694885+witnip@users.noreply.github.com>
2026-01-26 18:23:23 +0530

    feat: Update Gradle and build configurations
    - Upgrade Gradle wrapper from 8.14.4 to 9.1.0.
    - Add various Android Gradle Plugin flags to `gradle.properties` to configure build features, R class generation, and other build optimizations.
    - Simplify `compileSdk` definition in `app/build.gradle.kts`.
    - Update IDE deployment target settings.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,9 +6,7 @@ plugins {
 
 android {
     namespace = "com.codeturtle.subway_kiosk"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.codeturtle.subway_kiosk"
